06c949a63496817610c2a5215cac483b783d2ffd
jesus <60348759+jesus2307@users.noreply.github.com>
2021-03-03 12:05:13 +0100

    Update IAW-Practica-06back_end.sh

--- IAW-Practica-06back_end.sh
+++ IAW-Practica-06back_end.sh
@@ -3,37 +3,29 @@
 # ------------------------------------------------------------------------------
 # Instalación de la pila LAMP
 # ------------------------------------------------------------------------------
-# definimos la contraseña de root como variable
+# Definimos la contraseña de root como variable
 DB_ROOT_PASSWD=root
 DB_USU_PASSWD=usuario
+# Instalación y configuración de MySQL 
 
 # Habilitamos el modo de shell para mostrar los comandos que se ejecutan
 set -x
-
-# Actualizamos la lista de paquetes
-apt update
-
-
+# Actualizamos y actualizamos la lista de paquetes
+apt update  
 # Instalamos el sistema gestor de base de datos
 apt install mysql-server -y
 
-
-# Actualizamos la contraseña de root de MySQL
-mysql -u root <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '$DB_ROOT_PASSWD';"
-mysql -u root <<< "FLUSH PRIVILEGES;"
-
-#configuramos el parámetro bind-adress
-Sed -i ‘s/127.0.0.1/0.0.0.0/” etc/msql/msql.conf.d/mysql.cnf
-
-#reiniciar mysql
+# Editamos el archivo de configuración de MySQL, modificando la línea (Loop/cualquiera)
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf 
+# Reiniciamos el servicio
 sudo /etc/init.d/mysql restart
-
-# Instalación de la aplicación web propuesta
-#clonar el repositorio#
+# Clonamos el repositorio
 cd /home/ubuntu
 rm -rf iaw-practica-lamp 
 git clone https://github.com/josejuansanchez/iaw-practica-lamp
+# Actualizamos la contraseña de root de MySQL
+mysql -u root  <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '$DB_ROOT_PASSWD';"
+mysql -u root -p$DB_ROOT_PASSWD <<< "FLUSH PRIVILEGES;"
 
-
-# Introducimos la base de tados de la aplicación web
+# Introducimos la base de tados
 mysql -u root -p$DB_ROOT_PASSWD < /home/ubuntu/iaw-practica-lamp/db/database.sql
